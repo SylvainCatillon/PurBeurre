f29ce213bd06d265c58b9c41863bf6122080057f
SylvainCatillon <sylvain.catillon@gmail.com>
2019-10-09 17:40:37 +0200

    Creation of the tables of the database with the script

--- create_database_nolib.py
+++ create_database_nolib.py
@@ -1,13 +1,23 @@
 import requests as req
 import mysql.connector
+import config
+import pdb
 
 products = {}
 nb_pages = 1000//20
-categories = ["jus-d-orange"]
+#categories_tag = ["cereales-pour-petit-dejeuner", "Biscuits apéritifs", "chips"]
+categories_name = ["Céréales pour petit-déjeuner", "Biscuits apéritifs", "Chips", "Biscuits", "Pâtes à tartiner"]
 product_keys = ["product_name", "categories", "url", "nutrition_grade_fr", "purchase_places"]
+tables_list = [
+"Category (id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20))",
+"Product (id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50),\
+ category_id SMALLINT UNSIGNED, nutriscore CHAR(1), description TEXT, shop VARCHAR(50),\
+ url VARCHAR(100), CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES Category(id))",
+ "Favory (id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, product_id SMALLINT UNSIGNED,\
+  CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES Product(id))"]
 
 def fill_dict():
-	for category in categories:
+	for category in categories_name:
 		l = []
 		for i in range(nb_pages):
 			r = req.get("https://fr.openfoodfacts.org/categorie/{}/{}.json".format(category, str(i+1)))
@@ -25,15 +35,33 @@ def fill_dict():
 			l += result["products"]
 		products[category] = l
 
-"""def fill_db(products_dict):
-	connection = mysql.connector.connect(user= config.user, pasword = config.password, database = config.database, host = config.host)
+"""def create_db(cursor):
+	cursor.execute("CREATE DATABASE pur_beurre DEFAULT CHARACTER SET 'utf8'")"""
+
+def create_tables(cursor):
+	for table in tables_list:
+		request = "CREATE TABLE IF NOT EXISTS " + table + " ENGINE=INNODB"
+		print(request)
+		cursor.execute(request)
+
+def fill_tables(products_dict, cursor):
+	pass
+	"""cursor.execute("CREATE TABLE products (id SMALLINT UNSIGNED AUTO INCREMENT PRIMARY KEY, nom VARCHAR, category_id)")
+	sql = "INSERT INTO products (nom, category_id) VALUES (%s,%s)"
+	for product in products_dict:
+		cursor.execute(sql, (product["product_name"])), #product[category_id]))"""
+
+def fill_db(products_dict):
+	cnx = mysql.connector.connect(user= config.user, password = config.password, database = config.database, host = config.host)
+	cursor = cnx.cursor()
+	cursor.execute("USE pur_beurre")
 	try:
-		with connection.cursor() as cursor:
-			cursor.execute("CREATE TABLE products (id SMALLINT UNSIGNED AUTO INCREMENT PRIMARY KEY, nom VARCHAR, category_id)")
-			sql = "INSERT INTO products (nom, category_id) VALUES (%s,%s)"
-			for product in products_dict:
-				cursor.execute(sql, (product["product_name"], #product[category_id]))
-"""
+		create_tables(cursor)
+		fill_tables(products_dict, cursor)
+	finally:
+		cursor.close()
+		cnx.close()
+
 if __name__ == '__main__':
 	fill_dict()
 	print(len(products["jus-d-orange"]))
