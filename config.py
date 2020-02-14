3a30a91d9b6081c39e1d0cdd78715a54e38563e0
SylvainCatillon <sylvain.catillon@gmail.com>
2020-02-14 14:41:31 +0100

    Push config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+connection_config = dict(
+    user='pur_beurre_user',
+    password='pur_beurre_password',
+    database='pur_beurre',
+    host='localhost'
+)
+
+categories_name = [
+    "Céréales pour petit-déjeuner",
+    "Biscuits apéritifs",
+    "Chips",
+    "Biscuits",
+    "Pâtes à tartiner"]
+
+nb_proposed_prod = 10
+min_prod_in_db = 30 # Must be more than nb_proposed_prod
+nb_dl_prod = 800
